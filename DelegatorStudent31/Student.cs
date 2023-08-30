d12c776e13f1c92fd4bbdddbcb7cf90b1388c34a
Gihun Ham <ham@e-mirim.hs.kr>
2023-08-30 11:54:16 +0900

    Student 클래스 생성

---
+++ DelegatorStudent31/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatorStudent31
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public double Score { get; set; }
+        public Student( string name, double score) {
+            Name = name;
+            Score = score;
+        }
+        public override string ToString()
+        {
+            return Name + " : " + Score;
+        }
+    }
+}
